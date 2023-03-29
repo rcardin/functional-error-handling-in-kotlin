00eff20e181b8811bf882ff01e03cea020fc3dd8
rcardin <riccardo.cardin@gmail.com>
2023-03-29 21:46:02 +0200

    Added an example of exceptions breaking the RT of functions

--- src/main/kotlin/in/rcard/FunctionalErrorHandling.kt
+++ src/main/kotlin/in/rcard/FunctionalErrorHandling.kt
@@ -1,4 +1,28 @@
 package `in`.rcard
 
 fun main() {
+    val companiesService = CompaniesService(FakeCompanies())
+    val availableCompaniesNames = companiesService.getAvailableCompaniesNames()
+    println("Available companies are: $availableCompaniesNames")
 }
+
+interface Companies {
+    fun findAll(): List<Company>
+}
+
+class FakeCompanies : Companies {
+    override fun findAll(): List<Company> = throw RuntimeException("Boom!")
+}
+
+class CompaniesService(private val companies: Companies) {
+    fun getAvailableCompaniesNames(): List<String> {
+        val retrievedCompanies = companies.findAll()
+        return try {
+            retrievedCompanies.map { it.name }
+        } catch (e: Exception) {
+            listOf()
+        }
+    }
+}
+
+data class Company(val name: String)
