ea3a98a2d5279848e16e1b714ed37b2e26459960
rcardin <riccardo.cardin@gmail.com>
2023-04-07 11:37:59 +0200

    Started to review the examples made so far.

---
+++ src/main/kotlin/in/rcard/domain/JobDomain.kt
@@ -0,0 +1,17 @@
+package `in`.rcard.domain
+
+data class Job(val id: JobId, val company: Company, val role: Role, val salary: Salary)
+
+@JvmInline
+value class JobId(val value: Long)
+
+@JvmInline
+value class Company(val name: String)
+
+@JvmInline
+value class Role(val name: String)
+
+@JvmInline
+value class Salary(val value: Double) {
+    operator fun compareTo(other: Salary): Int = value.compareTo(other.value)
+}
--- src/main/kotlin/in/rcard/option/OptionErrorHandling.kt
+++ src/main/kotlin/in/rcard/option/OptionErrorHandling.kt
@@ -1,29 +1,26 @@
 package `in`.rcard.option
 
-import arrow.core.None
 import arrow.core.Option
-import arrow.core.Some
-import arrow.core.none
-import arrow.core.some
-import arrow.core.toOption
-import `in`.rcard.Company
-import `in`.rcard.Job
-import `in`.rcard.Role
-import `in`.rcard.Salary
+import `in`.rcard.domain.Job
+import `in`.rcard.domain.JobId
 
-val rockTheJvmJob: Some<Job> =
-    Some(Job(Company("Rock The Jvm"), Role("Technical Writer"), Salary(100_000.00)))
-val noJob: None = None
+// val rockTheJvmJob: Some<Job> =
+//    Some(Job(Company("Rock The Jvm"), Role("Technical Writer"), Salary(100_000.00)))
+// val noJob: None = None
+//
+// val appleJob: Option<Job> =
+//    Job(Company("Apple, Inc."), Role("Software Engineer"), Salary(70_000.00)).some()
+// val noAppleJob: Option<Job> = none()
+//
+// val microsoftJob: Job? =
+//    Job(Company("Microsoft"), Role("Software Engineer"), Salary(80_000.00))
+// val maybeMsJob: Option<Job> = Option.fromNullable(microsoftJob)
+// val noMsJob: Option<Job> = Option.fromNullable(null)
+//
+// val googleJob: Option<Job> =
+//    Job(Company("Google"), Role("Software Engineer"), Salary(90_000.00)).toOption()
+// val noGoogleJob: Option<Job> = null.toOption()
 
-val appleJob: Option<Job> =
-    Job(Company("Apple, Inc."), Role("Software Engineer"), Salary(70_000.00)).some()
-val noAppleJob: Option<Job> = none()
-
-val microsoftJob: Job? =
-    Job(Company("Microsoft"), Role("Software Engineer"), Salary(80_000.00))
-val maybeMsJob: Option<Job> = Option.fromNullable(microsoftJob)
-val noMsJob: Option<Job> = Option.fromNullable(null)
-
-val googleJob: Option<Job> =
-    Job(Company("Google"), Role("Software Engineer"), Salary(90_000.00)).toOption()
-val noGoogleJob: Option<Job> = null.toOption()
+interface Jobs {
+    fun findById(id: JobId): Option<Job>
+}
