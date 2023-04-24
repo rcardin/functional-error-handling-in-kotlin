239bedfb2c6e556470dd5569ec509899518b1130
rcardin <riccardo.cardin@gmail.com>
2023-04-24 15:17:47 +0200

    Fixed the JOBS_DATABASE contained values

--- src/main/kotlin/in/rcard/domain/JobDomain.kt
+++ src/main/kotlin/in/rcard/domain/JobDomain.kt
@@ -2,19 +2,19 @@ package `in`.rcard.domain
 
 val JOBS_DATABASE: Map<JobId, Job> = mapOf(
     JobId(1) to Job(
-        JobId(2),
+        JobId(1),
         Company("Apple, Inc."),
         Role("Software Engineer"),
         Salary(70_000.00),
     ),
     JobId(2) to Job(
-        JobId(3),
+        JobId(2),
         Company("Microsoft"),
         Role("Software Engineer"),
         Salary(80_000.00),
     ),
     JobId(3) to Job(
-        JobId(4),
+        JobId(3),
         Company("Google"),
         Role("Software Engineer"),
         Salary(90_000.00),
