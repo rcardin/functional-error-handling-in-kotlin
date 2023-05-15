f60a66ead2eac05c99930db0c710b5572e1c9f6e
rcardin <riccardo.cardin@gmail.com>
2023-05-15 09:53:01 +0200

    Added examples of Either creation using constructors

--- src/main/kotlin/in/rcard/either/EitherErrorHandling.kt
+++ src/main/kotlin/in/rcard/either/EitherErrorHandling.kt
@@ -1,1 +1,16 @@
 package `in`.rcard.either
+
+import arrow.core.Either
+import arrow.core.Either.Left
+import arrow.core.Either.Right
+import `in`.rcard.domain.JOBS_DATABASE
+import `in`.rcard.domain.Job
+import `in`.rcard.domain.JobId
+
+sealed interface JobError
+data class JobNotFound(val jobId: JobId) : JobError
+data class GenericError(val cause: String) : JobError
+
+val appleJobId = JobId(1)
+val appleJob: Either<JobError, Job> = Right(JOBS_DATABASE[appleJobId]!!)
+val jobNotFound: Either<JobError, JobNotFound> = Left(JobNotFound(appleJobId))
