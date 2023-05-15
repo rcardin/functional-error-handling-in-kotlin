ad08d92c83bbe9987acf91afa5e4d10daef6ca6e
rcardin <riccardo.cardin@gmail.com>
2023-05-15 21:24:39 +0200

    Added examples of Either construction using extension functions

--- src/main/kotlin/in/rcard/either/EitherErrorHandling.kt
+++ src/main/kotlin/in/rcard/either/EitherErrorHandling.kt
@@ -3,6 +3,8 @@ package `in`.rcard.either
 import arrow.core.Either
 import arrow.core.Either.Left
 import arrow.core.Either.Right
+import arrow.core.left
+import arrow.core.right
 import `in`.rcard.domain.JOBS_DATABASE
 import `in`.rcard.domain.Job
 import `in`.rcard.domain.JobId
@@ -14,3 +16,6 @@ data class GenericError(val cause: String) : JobError
 val appleJobId = JobId(1)
 val appleJob: Either<JobError, Job> = Right(JOBS_DATABASE[appleJobId]!!)
 val jobNotFound: Either<JobError, JobNotFound> = Left(JobNotFound(appleJobId))
+
+val anotherAppleJob = JOBS_DATABASE[appleJobId]!!.right()
+val anotherJobNotFound: Either<JobError, JobNotFound> = JobNotFound(appleJobId).left()
