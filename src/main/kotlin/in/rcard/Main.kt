10ee2fda16b2fcb5bd1041dd965a7f22525a82c1
rcardin <riccardo.cardin@gmail.com>
2023-05-25 22:12:27 +0200

    Used the new Salary type to demonstrate the ensure function

--- src/main/kotlin/in/rcard/Main.kt
+++ src/main/kotlin/in/rcard/Main.kt
@@ -1,9 +1,10 @@
 package `in`.rcard
 
 import arrow.core.Either
-import `in`.rcard.domain.Salary
+import `in`.rcard.either.EitherJobDomain.Salary
 import `in`.rcard.either.JobError
 
 fun main() {
-    val saalryOrError: Either<JobError, Salary> = Salary(-1.0)
+    val salaryOrError: Either<JobError, Salary> = Salary(-1.0)
+    println(salaryOrError)
 }
