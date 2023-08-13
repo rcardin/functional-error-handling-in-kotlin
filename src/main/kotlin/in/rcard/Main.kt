b4993712fe8db78d6a0dad74cc0827fea67c17af
rcardin <riccardo.cardin@gmail.com>
2023-08-13 20:55:13 +0200

    Added a description to execution of the getSalaryGapWithMax function

--- src/main/kotlin/in/rcard/Main.kt
+++ src/main/kotlin/in/rcard/Main.kt
@@ -1,13 +1,14 @@
 package `in`.rcard
 
-import arrow.core.raise.recover
-import `in`.rcard.either.JobError
+import arrow.core.raise.fold
+import `in`.rcard.domain.JobId
 import `in`.rcard.raise.CurrencyConverter
-import `in`.rcard.raise.RaiseCurrencyConverter
+import `in`.rcard.raise.JobsService
+import `in`.rcard.raise.LiveJobs
 
 fun main() {
-    val converter = RaiseCurrencyConverter(CurrencyConverter())
-    recover({ converter.convertUsdToEur(-1.0) }) { _: JobError ->
-        0.0
-    }
+    val service = JobsService(LiveJobs(), CurrencyConverter())
+    fold({ service.getSalaryGapWithMax(JobId(42)) },
+        { error -> println("An error was raised: $error") },
+        { salaryGap -> println("The salary gap is $salaryGap") })
 }
