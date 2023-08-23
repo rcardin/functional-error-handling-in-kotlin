cf073b327811257cfa30e0ca4f0f4c516cf1cc8c
rcardin <riccardo.cardin@gmail.com>
2023-08-23 21:24:02 +0200

    Used the mapOrAccumulate that is an extension on the Iterable

--- src/main/kotlin/in/rcard/raise/RaiseErrorHandling.kt
+++ src/main/kotlin/in/rcard/raise/RaiseErrorHandling.kt
@@ -1,17 +1,16 @@
 package `in`.rcard.raise
 
 import arrow.core.Either
-import arrow.core.NonEmptyList
 import arrow.core.None
 import arrow.core.Option
 import arrow.core.raise.NullableRaise
 import arrow.core.raise.OptionRaise
 import arrow.core.raise.Raise
+import arrow.core.raise.RaiseAccumulate
 import arrow.core.raise.ResultRaise
 import arrow.core.raise.catch
 import arrow.core.raise.either
 import arrow.core.raise.fold
-import arrow.core.raise.mapOrAccumulate
 import arrow.core.raise.nullable
 import arrow.core.raise.option
 import arrow.core.raise.withError
@@ -124,9 +123,9 @@ class JobsService(private val jobs: Jobs, private val converter: RaiseCurrencyCo
         }
     }
 
-    context (Raise<NonEmptyList<JobError>>)
+    context (RaiseAccumulate<JobError>)
     fun getSalaryGapWithMax(jobIdList: List<JobId>): List<Double> =
-        mapOrAccumulate(jobIdList) { getSalaryGapWithMax(it) }
+        jobIdList.mapOrAccumulate{ getSalaryGapWithMax(it) }
 }
 
 interface Jobs {
